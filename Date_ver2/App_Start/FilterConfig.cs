d0aef19ac5dd806062201632e9253777ac842d2b
CoventFury <rjumpadxd@gmail.com>
2023-08-10 09:25:13 +0800

    .

---
+++ Date_ver2/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Date_ver2
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Date_ver2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Date_ver2.MvcApplication" Language="C#" %>
